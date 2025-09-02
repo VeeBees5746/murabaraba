2c872a3cacc2f177846b396e2d6c0086ee0eab3b
VeeBees5746 <136152578+VeeBees5746@users.noreply.github.com>
2025-09-02 18:17:07 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,21 @@
-
+{
+  "content": [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  "theme": {
+    "fontFamily": {
+      "sans": ["Montserrat", "ui-sans-serif", "system-ui"]
+    },
+    "extend": {
+      "colors": {
+        "premium-orange": "#fbbf24",
+        "premium-blue": "#3b82f6",
+        "premium-board": "#fff7ed"
+      },
+      "boxShadow": {
+        "premium": "0 8px 32px 0 rgba(236, 128, 35, 0.18)"
+      }
+    }
+  },
+  "plugins": []
+}
